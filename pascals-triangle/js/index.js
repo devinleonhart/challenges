f2a3bfdbd0e808b7a60f860d53ed4370580c2026
Devin Leonhart <devinleonhart@gmail.com>
2019-01-12 23:40:25 -0800

    Bugfix and spelling error.

--- pascals-triangle/js/index.js
+++ pascals-triangle/js/index.js
@@ -4,16 +4,16 @@ let triangle = new Array();
 triangle[0] = [1];
 triangle[1] = [1, 1];
 
-// How many levels should the trangle have?
+// How many levels should the triangle have?
 let levels = 7;
 
 for (i = 2; i < levels; i++) {
   triangle[i] = new Array();
   triangle[i].push(1)
-  for(j = 1; j < i - 1; j++) {
+  for(j = 1; j < i; j++) {
     triangle[i].push(triangle[i - 1][j] + triangle[i - 1][j - 1]);
   }
   triangle[i].push(1)
 }
 
-console.log(t);
+console.log(triangle);
