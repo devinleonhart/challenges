dcbc7f83242ca795d79e939f0481f40eac7cb821
Devin Leonhart <devinleonhart@gmail.com>
2019-08-05 13:51:31 -0700

    Clean up the roman numeral challenge.

--- decode-roman-numerals/js/index.js
+++ decode-roman-numerals/js/index.js
@@ -8,21 +8,15 @@
 
 // You can never have the same roman numeral more than three times in a row.
 // A smaller number in front of a larger number means subtraction. ex.  IV = 4
-  // You can only put one smaller number in front of another in this way. ex. IIV is not 3.
+//  You can only put one smaller number in front of another in this way. 
+//  ex. IIV is not 3.
 // A larger number in front of a smaller number means addition.  ex. VI = 6
-// Ones, Tens, Hundreds, Thousands and so on must be separate roman numerals. ex, to get 99 you would make "90 and 9" (XCIX) instead of "1 before 100" (IC)
+// Ones, Tens, Hundreds, Thousands and so on must be separate roman numerals. 
+//  ex, to get 99 you would make "90 and 9" (XCIX) instead of 
+//  "1 before 100" (IC)
 
-// Compare the weight of two letters.
-// Accepts two letters, returns "greater", "lesser", or "same".
-function compare(key, a, b) {
-  if (key[a] > key[b]) 
-    return "greater";
-  if (key[a] < key[b]) 
-    return "lesser";
-  return "same"
-}
-
-// Check if the next letter is acending, descending, or the same. Return false if there's no next letter for the given index.
+// Check if the next letter is acending, descending, or the same. 
+// Return false if there's no next letter for the given index.
 function checkNextLetter(key, input, index) {
   
   let currentLetter = input[index]; 
@@ -31,27 +25,26 @@ function checkNextLetter(key, input, index) {
   if(!currentLetter || !nextLetter) {
     return false;
   }
-
-  if(nextLetter !== 'undefined') {
-    if(compare(key, currentLetter, nextLetter) === "lesser") {
-      return "ascending";
-    }
-    if(compare(key, currentLetter, nextLetter) === "greater") {
-      return "descending";
-    }
-    return "same";
+  if(key[currentLetter] < key[nextLetter]) {
+    return "ascending";
   }
-  
+  else if(key[currentLetter] > key[nextLetter]) {
+    return "descending";
+  }
+  return "same";
 }
 
 // Parse a roman numeral into a decimal number.
 function parse(key, input) {
   let decimal = 0;
   for (i = 0; i < input.length; i++) {
+    // If the next letter is larger, 
+    // subtract its value from this letter's value.
     if(checkNextLetter(key, input, i) === "ascending") {
       decimal += (key[input[i+1]] - key[input[i]])
       i += 1;
     }
+    // Otherwise, just add it.
     else {
       decimal += key[input[i]];
     }
@@ -60,13 +53,8 @@ function parse(key, input) {
 }
 
 let key = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}; 
-let input = 'XCIX';
 
 // Unit tests
-compare(key, 'V', 'M') === 'lesser' ? console.log("Pass!") : console.error("Fail!");
-compare(key, 'X', 'I') === 'greater' ? console.log("Pass!") : console.error("Fail!");
-compare(key, 'I', 'I') === 'same' ? console.log("Pass!") : console.error("Fail!");
-
 checkNextLetter(key, 'XCIX', 0) === "ascending" ? console.log("Pass!") : console.error("Fail!");
 checkNextLetter(key, 'XCIX', 1) === "descending" ? console.log("Pass!") : console.error("Fail!");
 checkNextLetter(key, 'XCIX', 1000) === false ? console.log("Pass!") : console.error("Fail!");
