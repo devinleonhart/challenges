f06d948e2e069fc81f812cae8731ef43d5965952
Devin Leonhart <devinleonhart@fastmail.com>
2019-12-01 19:31:46 -0800

    Added part 2.

--- advent-of-code-2019/1/index.js
+++ advent-of-code-2019/1/index.js
@@ -1,12 +1,25 @@
+// Part 1
 // Given an amount of weights, calculate how much fuel will be needed to launch the weight into orbit.
 // The formula for fuel requirement is Math.floor(W/3) -2.
+
+// Part 2
+// Assume any fuel added also has weight.
+
+function calculateFuel(mass) {
+  let fuel = Math.floor(mass / 3) - 2;
+  return fuel > 0 ? fuel : 0;
+}
+
+function calculateExtraFuel(fuel) {
+  return fuel > 0 ? fuel + calculateExtraFuel(calculateFuel(fuel)) : fuel + 0
+}
 
 let inputs = require('./input.json');
 const modules = inputs.mass.map((value) => {
   return {"mass": value}
 });
-modules.forEach((module) => module.fuel = Math.floor(module.mass / 3) - 2);
+modules.forEach((module) => module.fuel = calculateFuel(module.mass));
 const fuelRequirement = modules.reduce((acc, curr) => {
-  return curr.fuel > 0 ? acc + curr.fuel : 0;
+  return acc + calculateExtraFuel(curr.fuel);
 }, 0);
 console.log(fuelRequirement);
