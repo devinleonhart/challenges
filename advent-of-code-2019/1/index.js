c67558a411fdcc183eaf9bedb9666513ea08405e
Devin Leonhart <devinleonhart@fastmail.com>
2019-12-01 19:00:05 -0800

    Update with provided inputs for mass.

--- advent-of-code-2019/1/index.js
+++ advent-of-code-2019/1/index.js
@@ -1,14 +1,12 @@
-const modules = [
-  {mass: 1},
-  {mass: 12},
-  {mass: 14},
-  {mass: 1969},
-  {mass: 100756},
-];
+// Given an amount of weights, calculate how much fuel will be needed to launch the weight into orbit.
+// The formula for fuel requirement is Math.floor(W/3) -2.
 
+let inputs = require('./input.json');
+const modules = inputs.mass.map((value) => {
+  return {"mass": value}
+});
 modules.forEach((module) => module.fuel = Math.floor(module.mass / 3) - 2);
 const fuelRequirement = modules.reduce((acc, curr) => {
   return curr.fuel > 0 ? acc + curr.fuel : 0;
 }, 0);
-
 console.log(fuelRequirement);
